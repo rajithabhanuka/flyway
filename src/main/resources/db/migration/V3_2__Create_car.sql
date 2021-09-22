e5f4b167d8ad4120cc0b01b736b8a7fffa98043d
Rajitha Bhanuka <rajitha.bhanuka@axiatadigitallabs.com>
2021-09-22 10:19:05 +0530

    v4

---
+++ src/main/resources/db/migration/V3_2__Create_car.sql
@@ -0,0 +1,5 @@
+CREATE TABLE car (
+                     id INT NOT NULL PRIMARY KEY,
+                     license_plate VARCHAR(255) NOT NULL,
+                     color VARCHAR(255) NOT NULL
+);
